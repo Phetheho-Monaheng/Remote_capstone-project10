58179f7aef0b1fe037bc944e65f951f6be65df23
Phetheho-Monaheng <monahengphetheho4@gmail.com>
2024-06-02 12:18:59 +0200

    Added docstrings on views.py

--- neon_band/views.py
+++ neon_band/views.py
@@ -1,28 +1,77 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-# neon_app/views.py
-from django.shortcuts import render
-
 def home(request):
+    """
+    Renders the home page.
+
+    Parameters:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The rendered home page.
+    """
     return render(request, 'home.html')
 
 def about(request):
+    """
+    Renders the about page.
+
+    Parameters:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The rendered about page.
+    """
     return render(request, 'about.html')
 
 def discography(request):
+    """
+    Renders the discography page.
+
+    Parameters:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The rendered discography page.
+    """
     return render(request, 'discography.html')
 
 def tour(request):
+    """
+    Renders the tour page.
+
+    Parameters:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The rendered tour page.
+    """
     return render(request, 'tour.html')
 
 def contact(request):
+    """
+    Renders the contact page.
+
+    Parameters:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The rendered contact page.
+    """
     return render(request, 'contact.html')
 
 def signup(request):
+    """
+    Handles user signup.
+
+    Parameters:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The rendered signup page or redirects to the login page.
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -34,4 +83,13 @@ def signup(request):
 
 @login_required
 def user_profile(request):
+    """
+    Renders the user profile page.
+
+    Parameters:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The rendered user profile page.
+    """
     return render(request, 'user_profile.html')

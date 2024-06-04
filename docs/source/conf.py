4699c974d22ccb3aa2033678dba68e7b72bd7704
Phetheho-Monaheng <monahengphetheho4@gmail.com>
2024-06-04 20:06:03 +0200

    modified conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'neom_nand.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'neon_band.settings'
 django.setup()
 # Configuration file for the Sphinx documentation builder.
 #

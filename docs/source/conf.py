49589eed55b62649618c4eb837dc8a2b3b8e4b55
Phetheho-Monaheng <monahengphetheho4@gmail.com>
2024-06-04 20:04:39 +0200

    modified conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,8 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'neom_nand.settings'
+django.setup()
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:

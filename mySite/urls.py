ce258c07a41ae92b27602154770f7c2c51a2bc29
Phetheho-Monaheng <monahengphetheho4@gmail.com>
2024-06-11 14:28:52 +0200

    Updated.

--- mySite/urls.py
+++ mySite/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path('neon_band/', include('neon_band.urls')),
+    path('/', include('neon_band.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- neon_band/urls.py
+++ neon_band/urls.py
@@ -1,9 +1,9 @@
-# neon_app/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('discography/', views.discography, name='discography'),
     path('tour/', views.tour, name='tour'),
